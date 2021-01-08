8f960770706625170d953ee8ba82b600bccd67ff
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2021-01-08 13:18:12 -0500

    first commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen',
+  },
+  {
+    id: 3,
+    img: 'https://m.media-amazon.com/images/I/91MKaGShPJL._AC_UY218_.jpg',
+    title: 'The Proudest Blue: A Story of Hijab and Family',
+    author: 'Ibtihaj Muhammad',
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//css needs to have css extension
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { books } from './books'
+import SpecificBook from './Book'
+
+function Booklist() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>
+      })}
+    </section>
+  )
+}
+
+ReactDom.render(<Booklist />, document.getElementById('root'))
